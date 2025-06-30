57343ead6f585db54233207895b0ebcabab2f4e2
Mustapha abubakar iliyasu <141658189+TEA-BOY13@users.noreply.github.com>
2025-07-01 00:28:04 +0100

    Delete fetch and display directory

--- fetch and display/ListComponent.jsx
+++
@@ -1,9 +0,0 @@
-function ListComponent({ items, renderItem }) {
-  return (
-    <ul>
-      {items.map((item, index) => renderItem(item, index))}
-    </ul>
-  );
-}
-
-export default ListComponent;
